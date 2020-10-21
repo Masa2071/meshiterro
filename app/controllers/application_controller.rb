bb155a68547e80902cbae55678ecc234edbc4048
EC2 Default User <ec2-user@ip-172-31-45-58.ec2.internal>
2020-10-21 03:33:35 +0000

    [Add]画像投稿機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  # devise機能を利用前に、configure_permitted_parameresが実行される
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  # 〇〇のデータ操作を許可するアクションメソッド
+  # 今回は、ユーザー登録(sign_up)の際に、ユーザー名(:name)のデータ操作が可能
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd60c24e74db30cf10db7e8f5fc2019222013d3d6c18567db778fb2dacc7df0a0a9cdb8df81cd7486829495e20da8aa548acf4a065303142a7b5efb3c6f28335f'
