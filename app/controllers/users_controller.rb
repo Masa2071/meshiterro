73246bad20dd43f03062d9d8c53c9237e99501a9
EC2 Default User <ec2-user@ip-172-31-45-58.ec2.internal>
2020-10-22 08:25:17 +0000

    [add]部分テンプレート

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,21 @@ class UsersController < ApplicationController
     @post_images = @user.post_images.page(params[:page]).reverse_order
     # このように記述することで、そのユーザ（@user）に関連付けられた投稿（.post_images）のみ、@post_imagesに渡すことができます。
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+    
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
 end
